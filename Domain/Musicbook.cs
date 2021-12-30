7acc6366de6b1de5085a96837407f696efccb85d
ramon arents <ramonarents@hotmail.com>
2021-12-30 14:49:20 +0100

    initial commit

---
+++ Domain/Musicbook.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Domain
+{
+    public class Musicbook
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Book { get; set; }
+        public string Catgory { get; set; }
+    }
+}
